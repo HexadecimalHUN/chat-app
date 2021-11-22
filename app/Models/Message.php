b245b832e10edd73a85bac3594556641397fbd92
HexadecimalHUN <sajtos0007@gmail.com>
2021-11-22 13:22:03 +0100

    Authentication Path and Message Model Implementation

---
+++ app/Models/Message.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Message extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['message'];
+
+    public function user()
+    {
+    return $this->belongsTo(User::class);
+    }
+}
+// app/Message.php
+
+/**
+ * Fields that are mass assignable
+ *
+ * @var array
+ */
+
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', 'ChatsController@index');
+
+Route::get('messages', 'ChatsController@fetchMessages');
+
+Route::post('messages', 'ChatsController@sendMessage');
